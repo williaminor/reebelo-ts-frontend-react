61572d9b7a7792431d7911f67b326afbf3c06dfa
William Hart <williamsamhart@gmail.com>
2024-09-11 08:26:58 -0400

    added: ProductForm to app component

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -5,7 +5,7 @@ import {
   TrackingInfo,
   OrderResponse,
   ProductsResponse,
-} from "../types";
+} from "../types/types";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
