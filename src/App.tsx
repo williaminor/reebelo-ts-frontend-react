9f841c23c7103f2aa14531c264f7f7cbedec2a66
William Hart <williamsamhart@gmail.com>
2024-09-11 10:41:00 -0400

    added: routing to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { Suspense } from "react";
 import "./App.css";
-import ProductForm from "./components/ProductForm";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-function App() {
+const CreateProduct = React.lazy(() => import("./components/CreateProduct"));
+const UpdateProduct = React.lazy(() => import("./components/UpdateProduct"));
+const ProductList = React.lazy(() => import("./components/ProductList"));
+
+const App: React.FC = () => {
   return (
-    <div>
-      <h1>Reebelo Case Study</h1>
-      <ProductForm />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/products" element={<ProductList />} />
+            <Route path="/create-product" element={<CreateProduct />} />
+            <Route path="/update-product/:id" element={<UpdateProduct />} />
+            <Route path="*" element={<Navigate to="/products" replace />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
